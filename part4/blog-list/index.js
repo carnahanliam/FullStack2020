188cffdd8c855947f7aa76e1ab314a082e31c4a6
Liam Carnahan <carnahanliam13@gmail.com>
2021-05-15 16:21:21 -0400

    4.2 refactor application into separate modules

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,60 +1,33 @@
-// const app = require("./app");
-// const http = require("http");
-// const config = require("./utils/logger");
-
+const app = require("./app");
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
 
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// require("dotenv").config();
 
-const Blog = mongoose.model("Blog", blogSchema);
+// const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+// const mongoUrl = process.env.MONGODB_URI;
+// mongoose.connect(mongoUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((savedBlog) => savedBlog.toJSON())
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
