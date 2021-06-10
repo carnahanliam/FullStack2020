8456787c10e961a288849920ee05efdf1483b8e2
Liam Carnahan <carnahanliam13@gmail.com>
2021-06-09 21:15:15 -0400

    added exercises 4.13-4.14

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require("express").Router();
+const { request } = require("express");
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (request, response) => {
@@ -18,6 +19,24 @@ blogsRouter.post("/", async (request, response) => {
 
   const savedBlog = await blog.save();
   response.json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
